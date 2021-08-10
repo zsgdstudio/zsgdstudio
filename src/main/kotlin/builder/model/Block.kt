acf131269992b4de95fae4fa6827ae1accff1f07
zsgdstudio <zsgdstudio@gmail.com>
2021-08-10 18:20:20 +0300

    fix block parse priority

--- src/main/kotlin/builder/model/Block.kt
+++ src/main/kotlin/builder/model/Block.kt
@@ -13,10 +13,10 @@ fun blocks(context: Context, curPage: Page, fromLines: Flow<String>): List<Block
         val peekLine = fromLines.peek() ?: ""
         when {
             peekLine.isBlank() -> {fromLines.next(); continue}
-            isTableBlockLine(peekLine) -> subBlocks.add(TableBlock(context, curPage, fromLines))
             isQuoteBlockLine(peekLine) -> subBlocks.add(QuoteBlock(context, curPage, fromLines))
             isHeaderBlockLine(peekLine) -> subBlocks.add(HeaderBlock(context, curPage, fromLines))
             isOrderedListFirstLine(peekLine) || isUnorderedListFirstLine(peekLine) -> subBlocks.add(ListBlock(context, curPage, fromLines))
+            isTableBlockLine(peekLine) -> subBlocks.add(TableBlock(context, curPage, fromLines))
             else -> subBlocks.add(PlainBlock(context, curPage, fromLines))
         }
     }
